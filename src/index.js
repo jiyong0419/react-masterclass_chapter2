3a5051fcd407dea5025b90ace3d9d7e08fa9f1ef
jiyong0419 <wlfyd0419@gmail.com>
2022-11-19 22:38:24 +0900

    #2.7
    styled-component에서 theme을 사용하는법

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  bgColor: "#111",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
